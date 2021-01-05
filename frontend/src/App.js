7bebb37ad048d56d82876fe6c09ae33b68906ef6
Matheus Piffer <matheus.piffer.bc@gmail.com>
2021-01-04 22:59:38 -0300

    [ADD] - login screen and login in redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,17 @@ import Footer from "./components/Footer";
 import Home from "./views/Home/Home";
 import Product from "./views/Product/Product";
 import Cart from "./views/Cart/Cart";
+import Login from "./views/Login/Login";
 function App() {
   return (
     <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <Route path="/" component={Home} exact />
+          <Route path="/login" component={Login} exact />
           <Route path="/product/:id" component={Product} exact />
           <Route path="/cart/:id?" component={Cart} exact />
+          <Route path="/" component={Home} exact />
         </Container>
       </main>
       <Footer></Footer>
